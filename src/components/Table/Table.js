b501082c4b47f62e20e6283547e92df658a0bf56
hrz07 <hridoy.developer@yahoo.com>
2022-10-12 21:42:22 +0600

    item delete function done

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -10,17 +10,25 @@ const Table = () => {
             id: uuidv4(),
             amount: ""
         }
-
         setItem([...item, newItem])
-
     }
+
+    const handleClear = () => {
+        setItem([])
+    }
+
+    const handleDelete = (id) => {
+        let restItem = item.filter(item=> item.id !== id)
+        setItem(restItem)
+    }
+
 
     return (
         <div className="container">
             <div className='btnBox'>
                 <button onClick={createRow}>Insert New Row</button>
-                <button>Clear Row</button>
-                <button>Submit</button>
+                <button onClick={handleClear}>Clear Row</button>
+               {item.length > 0 && <button>Submit</button>}
             </div>
             <div className="tableContainer">
                 <table border={1} cellPadding={10}>
@@ -41,7 +49,7 @@ const Table = () => {
                                     <input type="text" />
                                 </td>
                                 <td>
-                                    <button>Delete</button>
+                                    <button onClick={()=>handleDelete(item.id)}>Delete</button>
                                 </td>
                             </tr>
                         })
